db1773b9c78326205a2fc15ff8fbe0d5812a2dfa
Alice Chen <alicew.chen@mail.utoronto.ca>
2021-01-05 16:36:36 -0500

    converts time in ms to UTC

---
+++ scripts/UTC_time_converter.py
@@ -0,0 +1,64 @@
+#originally from https://sammchardy.github.io/binance/2018/01/08/historical-data-download-binance.html
+#with minor edits
+
+# requires dateparser package
+import dateparser
+import pytz
+from datetime import datetime
+
+
+def date_to_milliseconds(date_str):
+    """Convert UTC date to milliseconds
+    If using offset strings add "UTC" to date string e.g. "now UTC", "11 hours ago UTC"
+    See dateparse docs for formats http://dateparser.readthedocs.io/en/latest/
+    :param date_str: date in readable format, i.e. "January 01, 2018", "11 hours ago UTC", "now UTC"
+    :type date_str: str
+    """
+    # get epoch value in UTC
+    epoch = datetime.utcfromtimestamp(0).replace(tzinfo=pytz.utc)
+    # parse our date string
+    d = dateparser.parse(date_str)
+    # if the date is not timezone aware apply UTC timezone
+    if d.tzinfo is None or d.tzinfo.utcoffset(d) is None:
+        d = d.replace(tzinfo=pytz.utc)
+
+    # return the difference in time
+    return int((d - epoch).total_seconds() * 1000.0)
+
+def interval_to_milliseconds(interval):
+    """Convert a Binance interval string to milliseconds
+    :param interval: Binance interval string 1m, 3m, 5m, 15m, 30m, 1h, 2h, 4h, 6h, 8h, 12h, 1d, 3d, 1w
+    :type interval: str
+    :return:
+         None if unit not one of m, h, d or w
+         None if string not in correct format
+         int value of interval in milliseconds
+    """
+    ms = None
+    seconds_per_unit = {
+        "m": 60,
+        "h": 60 * 60,
+        "d": 24 * 60 * 60,
+        "w": 7 * 24 * 60 * 60
+    }
+
+    unit = interval[-1]
+    if unit in seconds_per_unit:
+        try:
+            ms = int(interval[:-1]) * seconds_per_unit[unit] * 1000
+        except ValueError:
+            pass
+    return ms
+
+def milliseconds_to_date(utc_ms_ts, tz_info = pytz.timezone("America/Toronto")):
+    """Given millisecond utc timestamp and a timezone return dateime
+
+    :param utc_ms_ts: Unix UTC timestamp in milliseconds
+    :param tz_info: timezone info
+    :return: timezone aware datetime
+    """
+    # convert from time stamp to datetime
+    utc_datetime = datetime.utcfromtimestamp(utc_ms_ts / 1000.)
+
+    # set the timezone to UTC, and then convert to desired timezone
+    return utc_datetime.replace(tzinfo=pytz.timezone('UTC')).astimezone(tz_info)
